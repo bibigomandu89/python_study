cb3764fce5ab321c5de3f6ee091b0633844ec3c0
junhyun <chchakch@naver.com>
2025-07-04 16:44:20 +0900

    Update hw14.py
    x 와 y 를 좀더 직관적인 Height , age 로 변경

--- python/hw14.py
+++ python/hw14.py
@@ -1,7 +1,7 @@
-x = int(input("키를 입력하세요."))
-y = int (input("나이를 입력하세요."))
+H = int(input("키를 입력하세요."))
+A = int (input("나이를 입력하세요."))
 
-if x>=140 and  y>=10:
+if T>=140 and  A>=10:
     print("탑승이 가능합니다.")
 else :
     print("탑승이 불가능 합니다.")
