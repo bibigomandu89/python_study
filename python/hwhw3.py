09065e9d3163a77f5ab0ed34f5c3691142136b1c
junhyun <chchakch@naver.com>
2025-07-05 23:32:50 +0900

    사각형 삼각형 원형 숙제 완료

---
+++ python/hwhw3.py
@@ -0,0 +1,13 @@
+#사용자에게서 하나의 문자를 입력받고 문자가 R r 이면 사각형  T 면 트리앵글 C 면 써클 
+
+x = input("하나의 문자를 입력해주세요")
+if x == "R" or x == "r":
+    print("rectangle")
+elif x == "T" or x == "t":
+    print("triangle")
+
+elif x == "C" or x == "c":
+    print("circle")
+
+else :
+    print("Unknown")
